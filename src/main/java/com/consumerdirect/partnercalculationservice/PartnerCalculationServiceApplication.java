94b95af5c80e8973644aa466b3dbccbf4e80ae88
stelansimonsz <stelan.slt@gmail.com>
2023-05-06 11:07:46 +0800

    project init

---
+++ src/main/java/com/consumerdirect/partnercalculationservice/PartnerCalculationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.consumerdirect.partnercalculationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerCalculationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnerCalculationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consumerdirect/partnercalculationservice/PartnerCalculationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumerdirect.partnercalculationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerCalculationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
